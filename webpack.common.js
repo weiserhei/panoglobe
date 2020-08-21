8ced34c5b65888718b3f5cc6c9fd78bae18aa5f2
weiserhei <falk@thiele-medien.de>
2020-08-21 06:35:27 +0200

    add content hash

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,7 @@ module.exports = {
         }),
     ],
     output: {
-        filename: "[name].bundle.js",
+        filename: "[name].[contenthash].bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
     optimization: {
