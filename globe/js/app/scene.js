b681889d3c11a4ba04552fc28d0e63fc61667502
weiserhei <falk@thiele-medien.de>
2020-09-03 19:33:09 +0200

    delete files after merge

--- globe/js/app/scene.js
+++
@@ -1,9 +0,0 @@
-/**
- * Setup the scene
- */
-define(["three"], function (THREE){
-
-    var scene = new THREE.Scene();
-    return scene;
-
-});
--- globe/js/lib/three.js
+++
@@ -1,8 +0,0 @@
-/**
- * For creation of the global in requirejs
- *
- * Please add all addins here (and the main.js)
- */
-define(["threeCore","OrbitObjectControls"], function (threeCore,OrbitObjectControls) {
-    return OrbitObjectControls;
-});
